cae3063a8056dd94f249ae26ed5737ed8d94696c
Nirchains <nirchains@gmail.com>
2020-01-20 11:13:51 +0100

    Cambios en diseño front - Select2

--- cms/public/js/web/custom-web.js
+++ cms/public/js/web/custom-web.js
@@ -1,7 +1,7 @@
 frappe.provide("cms.utils");
 
 $.extend(cms.utils, {
-    getUrlParameter: function getUrlParameter(sParam) {
+    getUrlParameter: function (sParam) {
         var sPageURL = window.location.search.substring(1),
             sURLVariables = sPageURL.split('&'),
             sParameterName,
@@ -14,6 +14,29 @@ $.extend(cms.utils, {
                 return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
         }
+    },
+
+    insertURLParam: function(key, value)
+    {
+        key = encodeURI(key); value = encodeURI(value);
+
+        var kvp = document.location.search.substr(1).split('&');
+
+        var i=kvp.length; var x; while(i--) 
+        {
+            x = kvp[i].split('=');
+
+            if (x[0]==key)
+            {
+                x[1] = value;
+                kvp[i] = x.join('=');
+                break;
+            }
+        }
+
+        if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+        return kvp.join('&'); 
     },
 
     isElementInView: function (element, fullyInView) {
